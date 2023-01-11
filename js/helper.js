11cfea5e2b26bf55e001a5fd07c9d87aa089dd37
mohammad-equityzen <mohammad.nada@equityzen.com>
2023-01-11 15:46:31 +0300

    Build beauty card app

---
+++ js/helper.js
@@ -0,0 +1,63 @@
+function rgbToHex(col) {
+    if (col.charAt(0) == 'r') {
+        col = col.replace('rgb(', '').replace(')', '').split(',');
+        var r = parseInt(col[0], 10).toString(16);
+        var g = parseInt(col[1], 10).toString(16);
+        var b = parseInt(col[2], 10).toString(16);
+        r = r.length == 1 ? '0' + r : r; g = g.length == 1 ? '0' + g : g; b = b.length == 1 ? '0' + b : b;
+        var colHex = '#' + r + g + b;
+        return colHex;
+    }
+}
+function add_html_element({ html_tag, class_name = '', parent_element = document.body }) {
+    const html_element = document.createElement(html_tag);
+    if (class_name) {
+        html_element.classList.add(class_name)
+    }
+    parent_element.appendChild(html_element)
+    return html_element
+}
+function scale_card(element_id, selected_color_names) {
+    let card_element = document.getElementById(element_id)
+    let color_div = card_element.querySelector("div")
+    let button = document.querySelector(`[for='${element_id}']`)
+    let selected_number = Number(document.getElementById("number-of-selected").textContent)
+    let color_name = ntc.name(rgbToHex(color_div.style.backgroundColor))[1]
+    if (card_element.getAttribute("style")) {
+        if (!Object.values(card_element.style).indexOf("transform")) {
+            card_element.style.transform = null
+            button.style.backgroundColor = null
+            selected_number = selected_number - 1;
+            selected_color_names.splice(selected_color_names.indexOf(color_name), 1);
+        }
+    }
+    else {
+        card_element.style.transform = "scale(1.1, 1.04)";
+        button.style.backgroundColor = "#168000";
+        selected_number = selected_number + 1;
+        selected_color_names.push(color_name)
+    }
+    document.getElementById("number-of-selected").innerText = selected_number
+    let allColorsSelected = selected_color_names.join(" ")
+    document.getElementById("selected-colors").innerText = allColorsSelected
+}
+async function fetch_from_api(api_link) {
+    let response_data;
+    await axios.get(api_link)
+        .then(
+            (response) => {
+                response_data = response.data
+            }
+        ).catch((error) => {
+            console.log(error)
+        })
+    return response_data
+}
+function add_button_with_onclick_event({ onclick_fun: onclick_fun, parent_html: parent_html, attr_to_add: attr_to_add, selected_color_names: selected_color_names }) {
+    let button_element = document.createElement("button")
+    button_element.textContent += "Button"
+    parent_html.appendChild(button_element)
+    button_element.setAttribute(Object.keys(attr_to_add)[0], Object.values(attr_to_add)[0])
+    button_element.addEventListener("click", (e) => onclick_fun(e.target.getAttribute(Object.keys(attr_to_add)[0]), selected_color_names));
+}
+export { add_html_element, scale_card, fetch_from_api, add_button_with_onclick_event }
